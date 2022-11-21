6f9f8b2dfed0733d76a5b3638e29d34a7d7f2390
Faye Chen <faye2011@kimo.com>
2022-11-21 12:54:24 +1100

    implement nav bar logo (29:10)

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
+import Logo from './Logo'
+
+const Section = styled.section`
+width: 100vw;
+background-color: ${props => props.theme.body}
+`
+
+const NavBar = styled.nav`
+display: flex;
+justify-content: space-between;
+align-items: center;
+
+width: 85%;
+height: ${props => props.theme.navHeight};
+margin: 0 auto;
+`
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
+    <Section> 
+      <NavBar>
+        <Logo/>
+        <h2>Menu</h2>
+        <h2>Button</h2>
+      </NavBar>
+    </Section>
   )
 }
 
